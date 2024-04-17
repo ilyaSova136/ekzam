46fcbe75d3be29aad792c41f8b087fc17b9d3e8e
ilyaSova136 <123548714+ilyaSova136@users.noreply.github.com>
2024-04-17 08:56:12 +0300

    Add files via upload

---
+++ Exam/Form4.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Exam
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            maskedTextBox1 = new MaskedTextBox();
+            label1 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.AutoScroll = true;
+            flowLayoutPanel1.Location = new Point(1, 43);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(554, 408);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(213, 18);
+            maskedTextBox1.Mask = "00/00/0000";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(100, 23);
+            maskedTextBox1.TabIndex = 1;
+            maskedTextBox1.ValidatingType = typeof(DateTime);
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(213, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(85, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Поиск по дате";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(319, 17);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Поиск";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form4
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(556, 450);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(maskedTextBox1);
+            Controls.Add(flowLayoutPanel1);
+            MaximumSize = new Size(572, 489);
+            MinimumSize = new Size(572, 489);
+            Name = "Form4";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form4";
+            FormClosed += Form4_FormClosed;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private MaskedTextBox maskedTextBox1;
+        private Label label1;
+        private Button button1;
+    }
+}
